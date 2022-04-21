451a08831c48a80df2a68194a9f430805bab94b8
mazrou <ga.mazrou@gmail.com>
2022-04-21 14:46:46 +0100

    upgrade gradle version

--- app/src/main/java/com/mazrou/toDoApp/framework/presentation/components/StockItem.kt
+++ app/src/main/java/com/mazrou/toDoApp/framework/presentation/components/StockItem.kt
@@ -1,11 +1,8 @@
 package com.mazrou.toDoApp.framework.presentation.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,61 +15,48 @@ import com.mazrou.toDoApp.business.domain.models.Stock
 
 @Composable
 fun StockItem(
-    stock: Stock,
-    onClick: () -> Unit
+    stock: Stock, onClick: () -> Unit
 ) {
-    Card(
-       // shape = MaterialTheme.shapes.small,
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
-            .clickable { onClick() },
-        elevation = 2.dp
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .padding(horizontal = 16.dp , vertical = 5.dp)
-                .fillMaxWidth()
-                .fillMaxHeight()
+            .clickable { onClick() }
+            .border(1.dp, MaterialTheme.colors.secondary)
+            .padding(horizontal = 16.dp, vertical = 5.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()) {
+        // Stock ticker
+        Column(
+            modifier = Modifier.align(CenterVertically),
+            verticalArrangement = Arrangement.SpaceAround
         ) {
-            // Stock ticker
-            Column(
-                modifier = Modifier.align(CenterVertically),
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(
-                    text = stock.ticker,
-                    style = MaterialTheme.typography.h6,
-
-
-                )
-                Text(
-                    text = stock.ticker,
-                    style = MaterialTheme.typography.subtitle2,
-
-                )
-            }
-            //Pricing
-            Column(
-                modifier = Modifier.align(CenterVertically),
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    modifier  =Modifier.align(Alignment.End),
-                    text = stock.last.toString() + " $",
-                    style = MaterialTheme.typography.subtitle1,
-
-                )
-                Text(
-                    modifier  =Modifier.align(Alignment.CenterHorizontally),
-                    text = String.format("%.1f", (stock.last - stock.prevClose)) + "%",
-                    style = MaterialTheme.typography.subtitle2,
-                    color = if (stock.prevClose > stock.last) {
-                        Color.Red
-                    } else {
-                        Color.Green
-                    }
-                )
-            }
+            Text(
+                text = stock.ticker,
+                style = MaterialTheme.typography.h6,
+            )
+            Text(
+                text = stock.ticker,
+                style = MaterialTheme.typography.subtitle2,
+            )
+        }
+        //Pricing
+        Column(
+            modifier = Modifier.align(CenterVertically), verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.End),
+                text = stock.last.toString() + " $",
+                style = MaterialTheme.typography.subtitle1,
+            )
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = String.format("%.1f", (stock.last - stock.prevClose)) + "%",
+                style = MaterialTheme.typography.subtitle2,
+                color = if (stock.prevClose > stock.last) {
+                    Color.Red
+                } else {
+                    Color.Green
+                }
+            )
         }
     }
 }
