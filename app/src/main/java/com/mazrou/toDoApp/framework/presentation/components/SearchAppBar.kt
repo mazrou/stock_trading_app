b54f069a9fcd5208f5721b1c54e93537c14c9d05
mazrou <ga_mazrou@esi.dz>
2022-04-23 23:10:11 +0100

    fixe the search bar design

--- app/src/main/java/com/mazrou/toDoApp/framework/presentation/components/SearchAppBar.kt
+++ app/src/main/java/com/mazrou/toDoApp/framework/presentation/components/SearchAppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
@@ -78,12 +79,18 @@ fun SearchAppBar(
                             Icons.Filled.Clear,
                             contentDescription = "Clear icon",
                             modifier = Modifier
-                                //.offset(x = 10.dp)
                                 .clickable {
                                     onQueryChanged("")
+                                    onExecuteSearch()
+                                    keyboardController?.hide()
+                                    focusManager.clearFocus()
                                 }
                         )
-                    }
+                    },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFF929191),
+                        focusedLabelColor = Color(0xFF929191)
+                    )
                 )
             }
         }
