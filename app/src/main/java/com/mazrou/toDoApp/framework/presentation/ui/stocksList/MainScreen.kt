2cb1b7b215778086aed4924635596c21e2ff1e33
mazrou <ga_mazrou@esi.dz>
2022-04-23 23:12:11 +0100

    Add balance on the top of the screen

--- app/src/main/java/com/mazrou/toDoApp/framework/presentation/ui/stocksList/MainScreen.kt
+++ app/src/main/java/com/mazrou/toDoApp/framework/presentation/ui/stocksList/MainScreen.kt
@@ -1,11 +1,20 @@
 package com.mazrou.toDoApp.framework.presentation.ui.stocksList
 
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.SpaceBetween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.dp
 import com.mazrou.toDoApp.framework.presentation.components.SearchAppBar
 import com.mazrou.toDoApp.framework.presentation.components.StocksList
 import com.mazrou.toDoApp.framework.presentation.theme.AppTheme
@@ -31,13 +40,48 @@ fun MainScreen(
     ) {
         Scaffold(
             topBar = {
-                SearchAppBar(
-                    query = query,
-                    onQueryChanged = viewModel::onQueryChanged,
-                    onExecuteSearch = {
-                        //viewModel.onTriggerEvent(RecipeListEvent.NewSearchEvent)
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "My balance",
+                            modifier = Modifier
+                                .padding(16.dp, 16.dp, 0.dp, 0.dp),
+                            style = MaterialTheme.typography.h6,
+                            color = MaterialTheme.colors.onPrimary
+                        )
+                        Text(
+                            text = "$1000",
+                            modifier = Modifier
+                                .padding(0.dp, 16.dp, 16.dp, 0.dp),
+                            style = MaterialTheme.typography.h4,
+                            color = MaterialTheme.colors.onPrimary
+                        )
                     }
-                )
+                    Button(
+                        onClick = {}, modifier = Modifier
+                            .align(Alignment.End)
+                            .padding(10.dp)
+                            .background(MaterialTheme.colors.background)
+
+                    ) {
+                        Icon(Icons.Filled.Done, contentDescription = "See my trades")
+                        Text(text = "See my trades")
+                    }
+                    SearchAppBar(
+                        query = query,
+                        onQueryChanged = viewModel::onQueryChanged,
+                        onExecuteSearch = {
+                            //viewModel.onTriggerEvent(RecipeListEvent.NewSearchEvent)
+                        }
+                    )
+                }
             }
         ) {
             StocksList(
