c4dfbb4c1cb4edba34a74cf208c7398ab5c7f4f0
mazrou <ga_mazrou@esi.dz>
2022-04-25 14:55:57 +0100

    re-implement the getBalance method from fireStore

--- app/src/main/java/com/mazrou/toDoApp/framework/datasource/network/trades/TradeNetworkServiceImpl.kt
+++ app/src/main/java/com/mazrou/toDoApp/framework/datasource/network/trades/TradeNetworkServiceImpl.kt
@@ -1,30 +1,46 @@
 package com.mazrou.toDoApp.framework.datasource.network.trades
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.mazrou.toDoApp.framework.utils.cLog
 import kotlinx.coroutines.tasks.await
 
 class TradeNetworkServiceImpl(
-    private val fireStore: FirebaseFirestore
+    private val fireStore: FirebaseFirestore,
+    context: Context
 ) : TradeNetworkService {
 
 
+    @SuppressLint("HardwareIds")
+    val deviceID: String = Settings.Secure.getString(
+        context.contentResolver,
+        Settings.Secure.ANDROID_ID
+    )
+
     override suspend fun getBalance(): Double {
 
-        return fireStore
-            .collection("NOTES_COLLECTION")
-            .document("USER_ID")
-            .collection("NOTES_COLLECTION")
+       return  fireStore
+            .collection("Trades")
+            .document("dsiodfijsdf")
             .get()
             .addOnFailureListener {
                 // send error reports to Firebase Crashlytics
                 cLog(it.message)
             }.await()
-            .toObjects(Double::class.java)
-            .first()
+             .data?.let {
+               return try {
+                   it["balance"]  as Double
+               }catch (e : ClassCastException){
+                   (it["balance"] as Long).toDouble()
+               }
+           }?: kotlin.run {return 0.0}
     }
 
     override suspend fun setBalance(balance: Double) {
 
     }
+
 }
