ce9fb3892cefd63d4434abb159104c983040be3e
mazrou <ga_mazrou@esi.dz>
2022-04-28 15:45:42 +0100

    add trade history title

---
+++ app/src/main/java/com/mazrou/toDoApp/framework/presentation/components/TradeItem.kt
@@ -0,0 +1,63 @@
+package com.mazrou.toDoApp.framework.presentation.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.mazrou.toDoApp.business.domain.models.Trade
+import com.mazrou.toDoApp.business.domain.models.TradingType
+
+@Composable
+fun TradeItem(
+    trade: Trade
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .border(1.dp, MaterialTheme.colors.secondary)
+            .padding(horizontal = 16.dp, vertical = 5.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        // Stock ticker
+        Column(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(
+                text = trade.ticker,
+                style = MaterialTheme.typography.h6,
+            )
+            Text(
+                text = trade.type.name,
+                style = MaterialTheme.typography.subtitle2,
+                color = if (trade.type == TradingType.SELL) {
+                    Color.Red
+                } else {
+                    Color.Green
+                }
+            )
+        }
+        //Pricing
+        Column(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.End),
+                text = "$ " + trade.price.toString(),
+                style = MaterialTheme.typography.subtitle1,
+            )
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = trade.date.toString(),
+                style = MaterialTheme.typography.subtitle2,
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/mazrou/toDoApp/framework/presentation/components/TradeList.kt
@@ -0,0 +1,29 @@
+package com.mazrou.toDoApp.framework.presentation.components
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import com.mazrou.toDoApp.business.domain.models.Trade
+
+@ExperimentalUnitApi
+@Composable
+fun TradeList(
+    loading: Boolean,
+    trades: List<Trade>,
+    modifier: Modifier
+) {
+    if (loading && trades.isEmpty()) {
+        //TODO show the shimmer
+    } else if (trades.isEmpty()) {
+        NothingHere()
+    } else {
+        //FIXME this list is laggy . this is no my problem
+        LazyColumn(modifier) {
+            itemsIndexed(items = trades) { _: Int, item: Trade ->
+                TradeItem(trade = item)
+            }
+        }
+    }
+}
